c6ea939cb3f2d9c1cf993f7f2f6ad4cb8bb0d166
Benoît Daireaux <54928991+benoitdaireaux@users.noreply.github.com>
2024-10-29 12:53:19 +0100

    Inject and signal acquisition

---
+++ Samples/AcquireSignals/Program.cs
@@ -0,0 +1,65 @@
+﻿using DWIS.Client.ReferenceImplementation.OPCFoundation;
+using DWIS.Client.ReferenceImplementation;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using DWIS.SPARQL.Utils;
+using DWIS.Vocabulary.Schemas;
+using DWIS.Vocabulary.Development;
+
+var builder = Host.CreateDefaultBuilder();
+builder.ConfigureServices(services =>
+services.AddLogging(config => config.AddConsole().SetMinimumLevel(LogLevel.Information)));
+
+var host = builder.Build();
+var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
+var logger = loggerFactory.CreateLogger<Program>();
+
+
+
+var configuration = DefaultDWISClientConfiguration.LoadDefault();
+
+IOPCUADWISClient client = new DWISClientOPCF(configuration, loggerFactory.CreateLogger<DWISClientOPCF>());
+
+
+var subscriptionData = GetSubscriptionData();
+
+
+
+
+AcquiredSignals acquiredSignals = AcquiredSignals.CreateWithSubscription(subscriptionData.queries, subscriptionData.queryNames, 0, client);
+
+PeriodicTimer timer = new PeriodicTimer(TimeSpan.FromSeconds(.5));
+
+while (await timer.WaitForNextTickAsync())
+{
+    foreach (var data in acquiredSignals)
+    {
+        var val = data.Value;
+
+        Console.WriteLine($"Signal name: {data.Key}");
+
+        if (val != null)
+        { 
+        foreach(var signal in val)
+            {
+                double s = signal.GetValue<double>(double.NaN);
+                Console.WriteLine($"\t Value: {s}");
+            }
+        }
+    }
+}
+
+
+Console.ReadLine();
+
+(string[] queries, string[] queryNames) GetSubscriptionData()
+{
+    string[] nouns = new string[]{
+    Nouns.BitDepth,Nouns.HookPosition,Nouns.HookVelocity,Nouns.FlowRateIn,Nouns.SPP,Nouns.SurfaceRPM,Nouns.HoleDepth
+    };
+
+    var queries = nouns.Select(noun => { return new QueryBuilder().SelectSignal().SelectDataPoint().AddMeasuredClass(noun).Build(); }).ToArray();
+    var names = nouns;
+    return (queries, nouns);
+}
--- Samples/InjectManifestAndWriteData/Program.cs
+++ Samples/InjectManifestAndWriteData/Program.cs
@@ -1,2 +1,109 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DWIS.API.DTO;
+using DWIS.Client.ReferenceImplementation;
+using DWIS.Client.ReferenceImplementation.OPCFoundation;
+using DWIS.Vocabulary.Schemas;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+
+var builder = Host.CreateDefaultBuilder();
+builder.ConfigureServices(services => 
+services.AddLogging(config => config.AddConsole().SetMinimumLevel(LogLevel.Information)));
+
+var host = builder.Build();
+var loggerFactory = host.Services.GetRequiredService<ILoggerFactory>();
+var logger = loggerFactory.CreateLogger<Program>();
+
+
+
+var configuration = DefaultDWISClientConfiguration.LoadDefault();
+
+IOPCUADWISClient client = new DWISClientOPCF(configuration, loggerFactory.CreateLogger<DWISClientOPCF>());
+var manifest = GetManifest();
+
+
+logger.LogInformation("Inject manifest");
+var res = client.Inject(manifest);
+logger.LogInformation("Manifest injected");
+
+
+Random random = new Random();
+
+PeriodicTimer periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(.5));
+while (await periodicTimer.WaitForNextTickAsync()) 
+{
+    DateTime now = DateTime.Now;
+    var data = manifest.ProvidedVariables.Select(pv => (pv.VariableID,(object) random.NextDouble(), now)).ToList();
+    client.UpdateProvidedVariables(data);
+}
+
+Console.ReadLine();
+
+ManifestFile GetManifest()
+{
+    ManifestFile manifest = new ManifestFile()
+    {
+        InjectedNodes = new List<InjectedNode>(),
+        InjectedReferences = new List<InjectedReference>(),
+        InjectedVariables = new List<InjectedVariable>(),
+        InjectionInformation = new InjectionInformation(),
+        ProvidedVariables = new List<ProvidedVariable>(),
+        ManifestName = "Sample-manifest",
+        Provider = new InjectionProvider() { Company = "NORCE", Name = "SamplesApp" }
+    };
+    List<(string name, string prototype, string unit, string quantity)> injectionData = new List<(string name, string prototype, string unit, string quantity)>();
+
+
+    injectionData.Add((Nouns.HookLoad, Nouns.HookLoad, Units.Kilogram, Quantities.HookLoadDrillingQuantity));
+    injectionData.Add((Nouns.HookPosition, Nouns.HookPosition, Units.Metre, Quantities.PositionDrillingQuantity));
+    injectionData.Add((Nouns.HookVelocity, Nouns.HookVelocity, Units.MetrePerSecond, Quantities.BlockVelocityDrillingQuantity));
+    injectionData.Add((Nouns.FlowRateIn, Nouns.FlowRateIn, Units.CubicMetrePerSecond, Quantities.VolumetricFlowrateDrillingQuantity));
+    injectionData.Add((Nouns.SPP, Nouns.SPP, Units.Pascal, Quantities.PressureDrillingQuantity));
+    injectionData.Add((Nouns.SurfaceRPM, Nouns.SurfaceRPM, Units.RotationPerSecond, Quantities.RotationalFrequencyQuantity));
+    injectionData.Add((Nouns.BitDepth, Nouns.BitDepth, Units.Metre, Quantities.DepthDrillingQuantity));
+    injectionData.Add((Nouns.HoleDepth, Nouns.HoleDepth, Units.Metre, Quantities.DepthDrillingQuantity));
+
+    foreach (var injection in injectionData)
+    {
+        manifest.InjectedNodes.Add(new InjectedNode() { BrowseName = injection.name, DisplayName = injection.name, TypeDictionaryURI = Nouns.DrillingDataPoint, UniqueName = injection.name });
+        manifest.ProvidedVariables.Add(new ProvidedVariable() { DataType = "double", VariableID = injection.name });
+
+        manifest.InjectedReferences.Add(new InjectedReference()
+        {
+            Subject = new NodeIdentifier() { ID = injection.name, NameSpace = manifest.InjectionInformation.InjectedNodesNamespaceAlias },
+            VerbURI = "http://ddhub.no/" + Verbs.HasDynamicValue,
+            Object = new NodeIdentifier() { ID = injection.name, NameSpace = manifest.InjectionInformation.ProvidedVariablesNamespaceAlias }
+        });
+
+        manifest.InjectedReferences.Add(new InjectedReference()
+        {
+            Subject = new NodeIdentifier() { ID = injection.name, NameSpace = manifest.InjectionInformation.InjectedNodesNamespaceAlias },
+            VerbURI = "http://ddhub.no/" + Verbs.BelongsToClass,
+            Object = new NodeIdentifier() { ID = "http://ddhub.no/"+ injection.prototype, NameSpace = "http://ddhub.no/" }
+        });
+
+        manifest.InjectedReferences.Add(new InjectedReference()
+        {
+            Subject = new NodeIdentifier() { ID = injection.name, NameSpace = manifest.InjectionInformation.InjectedNodesNamespaceAlias },
+            VerbURI = "http://ddhub.no/" + Verbs.BelongsToClass,
+            Object = new NodeIdentifier() { ID = "http://ddhub.no/" + Nouns.Measurement, NameSpace = "http://ddhub.no/" }
+        });
+
+        manifest.InjectedReferences.Add(new InjectedReference()
+        {
+            Subject = new NodeIdentifier() { ID = injection.name, NameSpace = manifest.InjectionInformation.InjectedNodesNamespaceAlias },
+            VerbURI = "http://ddhub.no/" + Verbs.IsOfMeasurableQuantity,
+            Object = new NodeIdentifier() { ID = injection.quantity, NameSpace = "http://ddhub.no/UnitAndQuantity/" }
+        });
+        manifest.InjectedReferences.Add(new InjectedReference()
+        {
+            Subject = new NodeIdentifier() { ID = injection.name, NameSpace = manifest.InjectionInformation.ProvidedVariablesNamespaceAlias },
+            VerbURI = "http://ddhub.no/" + Verbs.HasUnitOfMeasure,
+            Object = new NodeIdentifier() { ID = injection.unit, NameSpace = "http://ddhub.no/UnitAndQuantity/" }
+        });
+    }
+
+    return manifest;
+}
+
